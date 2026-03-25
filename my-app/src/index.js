83f96bef263a70525404f83e9cd1dfb34e9bfc6f
AjaxKronus <107383559+AjaxKronus@users.noreply.github.com>
2026-03-24 23:46:39 -0700

    Add files via upload

---
+++ my-app/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import 'firebaseui/dist/firebaseui.css';
+import App from './components/App';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+
+// Our web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBw3NOPo_sTjzdoflksL1MEkSPJ0S2PtRc",
+  authDomain: "info340group-5.firebaseapp.com",
+  projectId: "info340group-5",
+  storageBucket: "info340group-5.appspot.com",
+  messagingSenderId: "292450042823",
+  appId: "1:292450042823:web:db53547a3f5b6514670fe2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App auth={auth} />
+    </BrowserRouter>
+  </React.StrictMode>
+);
